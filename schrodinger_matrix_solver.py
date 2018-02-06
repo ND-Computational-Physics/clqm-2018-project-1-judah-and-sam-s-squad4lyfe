9bafb37866de6445fa3fbf81d144fa0466973023
Judah <jvanzand@nd.edu>
2018-02-06 09:51:34 -0500

    New matrix solver to give eigenvalues and eigenfunctions.

---
+++ schrodinger_matrix_solver.py
@@ -0,0 +1,32 @@
+""" schrodinger_matrix_solver.py
+Language: Python3
+Sam Porter, Judah Van Zandt
+January 28, 2018
+Written for CLQM Schrodinger Project
+"""
+
+# The basis of this project is to convert the linear schrodinger eqaution into
+# an eigenvalue problem. This means we have to set up a modifiable Hamiltonian
+# matrix, as well as a way to solve for its eigenvalues.
+import numpy as np
+np.set_printoptions(precision=3)
+
+def random_matrix(n):
+
+    matrix = np.zeros((n,n))
+    for i in range(n):
+        for j in range(n):
+            matrix[i,j] = np.random.random()
+    return matrix
+
+def eigensolver(matrix):
+
+    eigenvalues = np.linalg.eig(matrix)[0]
+    eigenvectors = np.linalg.eig(matrix)[1]
+
+    return eigenvalues, eigenvectors
+
+if __name__ = "__main__"
+    print(eigensolver(random_matrix(3))[0])
+    print('')
+    print(eigensolver(random_matrix(3))[1])
--- schrodinger_to_matrix.py
+++ schrodinger_to_matrix.py
@@ -20,16 +20,16 @@ def V(x):
         x (float): Discrete values between the two endpoints of evaluation.
     """
     V_func = .5*x**2 # This is the potential function! Change it here!
-    
+
     return V_func
 
 
 class Schrod_Matrix:
-    def __init__(self,endpoints, num_steps, potential):
+    def __init__(self, endpoints, num_steps, potential):
         """
         Arguments:
             endpoints (list): The two endpoints of our range over which we're solving the Schrodinger Equation.
-            
+
             num_steps (int): The number of steps we're solving the Schrodinger Equation at, essentially the size of our hamiltonian matrix.
 
             potential (function): Our potential, resulting from V(x).
@@ -37,18 +37,18 @@ class Schrod_Matrix:
         self.endpoints = endpoints
         self.num_steps = num_steps
         self.potential = potential
-        
-    def matrix_element_generate(self,i,j, x_points):
+
+    def matrix_element_generate(self, i, j, x_points):
         """
         Generating the elements of our hamiltonian matrix.
 
         Arguments:
             i (index): Row index
-            
+
             j (index): Column index
         """
         h = 1
-        
+
         if i == j:
             element = (2/h**2) + self.potential(x_points[i])
         elif i >= j+2:
@@ -57,24 +57,24 @@ class Schrod_Matrix:
             element = 0
         else:
             element = -1/(h**2)
-        
+
         return element
-    
+
     def x_set(self):
         x = []
         step = (self.endpoints[1] - self.endpoints[0])/self.num_steps
-        
+
         for i in range(1,self.num_steps):
             x.append(self.endpoints[1] + i*step)
-        
+
         return x
-    
+
     def gen_matrix(self,x):
         hamilt = np.zeros([self.num_steps-1,self.num_steps-1])
-        
+
         for i in range(self.num_steps-1):
             for j in range(self.num_steps-1):
                 ele = self.matrix_element_generate(i,j,x)
                 hamilt[i][j] = ele
-        
+
         return hamilt
