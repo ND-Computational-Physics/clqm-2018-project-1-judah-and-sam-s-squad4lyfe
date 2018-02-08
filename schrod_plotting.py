0abd40aaaa74d405ec5d882b1f55cedcaae4c8f9
Unknown <wporter@nd.edu>
2018-02-08 01:16:18 -0500

    5th state for Schrodinger Solver functions
    Commented the code better, and appropriate changed the name of test.py to schrod_plotting.py

---
+++ schrod_plotting.py
@@ -0,0 +1,50 @@
+""" schrod_plotting.py
+
+    Testing and plotting solutions to our Schrodinger solver.
+
+    Language: Python 3
+
+    Sam Porter, Judah Van Zandt
+    University of Notre Dame
+
+    Written for Computational Lab in Quantum Mechanics, Spring 2018
+    Last updated on 2/8/2018
+"""
+import schrodinger_to_matrix as stm
+import numpy as np
+import schrodinger_matrix_solver as sms
+import matplotlib.pyplot as plt
+
+def schrod_plot(endpoints, num_points, potential, range_var1, range_var2): 
+    """
+    Plotting and normalizing our solutions to the Schrodinger Equation.
+    
+    Inputs:
+        endpoints (list): The endpoints at which we want to evaluate between
+        num_points (int): The number of points (i.e. solutions) we want to have
+        potental (func): The potential we want to consider when evaluating
+        range_var1 (int): The lower bound of the eigenvector solutions we want to plot
+        range_var2 (int): The upper bound of the eigenvector solutions we want to plot
+    
+    Outputs:
+        (plot): A plot of the normalized eigenvectors you're interested in plotting
+    """
+    test_case = stm.Schrod_Matrix(endpoints,num_points-1, potential)
+    x_val = test_case.x_set()
+    matrix = test_case.gen_matrix(x_val)
+    
+    matrix = .5*matrix # Our matrix is scaled by a factor of 2 too large based on the derivation of this method
+
+    eigenva,eigenve = sms.eigensolver(matrix)
+
+    eigenve = np.transpose(eigenve) # Transposing the eigenvectors so we can plot them against our x_val
+
+    for i in range(range_var1,range_var2):
+        eigenve1 = np.array(eigenve[i])**2 # Normalizing our eigenvectors
+        norm_factor = np.trapz(eigenve1,x_val)
+        eigenve_plot = np.array(eigenve[i])*np.sqrt(1/norm_factor)
+        plt.plot(x_val,eigenve_plot)
+
+    plt.show()
+
+schrod_plot([-5,5],201,stm.V, 14,15)
